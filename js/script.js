dd852abd0e59d84d7d3d578472e711cb323956e3
Ravicode9 <iamraviraj09@gmail.com>
2024-10-10 13:35:56 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+document.getElementById('farmer-register-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Farmer registered successfully!');
+});
+
+document.getElementById('buyer-register-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Buyer registered successfully!');
+});
 
